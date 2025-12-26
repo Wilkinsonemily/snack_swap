8d211eed8931a9498e728fe30ed8777b24e2ae4b
Wilkinsonemily <wilkinsonemilyy05@gmail.com>
2025-12-26 15:07:09 +0700

    Fix DatabaseSeeder password syntax

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'admin@toko.com'],
+            ['email' => 'admin@example.com'],
             [
                 'name' => 'Super Admin',
                 'password' => Hash::make('password123'),
